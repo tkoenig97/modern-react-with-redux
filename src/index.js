eb70fca2e1f85ef06b5040f2c194ec867bf8eea3
tkoenig97 <tylerskoenig@gmail.com>
2022-08-01 11:38:09 -0500

    Typical Component Layouts

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-    return <h1>{new Date().toLocaleTimeString()}</h1>;
+    const name = 'Samantha'
+    const age = 23
+
+    return (
+        <h1>
+            Hi, my name is {name} and my age is {age}
+        </h1>
+    );
 }
 
 // 5) Show the component on the screen
