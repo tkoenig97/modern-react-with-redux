8cfb9f4d601933746f8fa52877d24c5793006102
tkoenig97 <tylerskoenig@gmail.com>
2022-08-01 11:58:14 -0500

    Applying Styling to JSX

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-    return <textarea autoFocus={true}/>
+    return <input style={{border: '1px solid blue'}}/>
 }
 
 // 5) Show the component on the screen
